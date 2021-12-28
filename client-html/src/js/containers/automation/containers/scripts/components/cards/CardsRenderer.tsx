237d4f66aa42f4729956641dbc98ee4c88c9915b
5tronciy <romangolchuk@gmail.com>
2021-12-28 14:47:43 +0300

    feat: refactor to use virtualList

--- client-html/src/js/containers/automation/containers/scripts/components/cards/CardsRenderer.tsx
+++ client-html/src/js/containers/automation/containers/scripts/components/cards/CardsRenderer.tsx
@@ -5,6 +5,7 @@
 
 import * as React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+// import { FixedSizeList as List } from "react-window";
 import Grid from "@mui/material/Grid";
 import { FormControlLabel } from "@mui/material";
 import { WrappedFieldArrayProps } from "redux-form";
@@ -59,158 +60,141 @@ const CardsRenderer: React.FC<Props & WrappedFieldArrayProps> = props => {
 
   const renderVariables = (variables, name, disabled) => (
     <>
-      {variables.map(elem => (
+      {variables.map(elem =>
         elem.type === "Checkbox" ? (
           <Grid key={getType(elem.type) + elem.label} item xs={12}>
             <FormControlLabel
-              control={(
-                <FormField
-                  type={elem.type.toLowerCase()}
-                  name={`${name}.${elem.name}`}
-                  label={elem.label}
-                />
-              )}
+              control={<FormField type={elem.type.toLowerCase()} name={`${name}.${elem.name}`} label={elem.label} />}
               disabled={disabled}
               label={elem.label}
             />
           </Grid>
         ) : (
           <Grid key={getType(elem.type) + elem.label} item xs={12}>
-            <FormField
-              type={getType(elem.type)}
-              name={`${name}.${elem.name}`}
-              label={elem.label}
-              disabled={disabled}
-              required
-            />
+            <FormField type={getType(elem.type)} name={`${name}.${elem.name}`} label={elem.label} disabled={disabled} required />
           </Grid>
         )
-      ))}
+      )}
     </>
-);
+  );
+
+  const Item = ({ component, provided, index, item }) => {
+    switch (component.type) {
+      case "Script": {
+        return (
+          <div ref={provided.innerRef} {...provided.draggableProps}>
+            <ScriptCard
+              heading="Script"
+              className="mb-3"
+              onDelete={!isInternal && hasUpdateAccess ? e => onDelete(e, index) : null}
+              dragHandlerProps={provided.dragHandleProps}
+              expanded
+              noPadding
+              onDetailsClick={isInternal ? onInternalSaveClick : undefined}
+            >
+              <FormField type="code" name={`${item}.content`} disabled={isInternal || !hasUpdateAccess} className="mt-3" />
+            </ScriptCard>
+          </div>
+        );
+      }
+      case "Query": {
+        return (
+          <div ref={provided.innerRef} {...provided.draggableProps}>
+            <ScriptCard
+              heading="Query"
+              className="mb-3"
+              onDelete={!isInternal ? e => onDelete(e, index) : null}
+              dragHandlerProps={provided.dragHandleProps}
+              expanded
+              onDetailsClick={isInternal ? onInternalSaveClick : undefined}
+            >
+              <QueryCardContent dispatch={dispatch} field={component} name={item} classes={classes} disabled={isInternal} />
+            </ScriptCard>
+          </div>
+        );
+      }
+      case "Message": {
+        return (
+          <div ref={provided.innerRef} {...provided.draggableProps}>
+            <ScriptCard
+              heading="Message"
+              className="mb-3"
+              onDelete={!isInternal ? e => onDelete(e, index) : null}
+              dragHandlerProps={provided.dragHandleProps}
+              expanded
+              onDetailsClick={isInternal ? onInternalSaveClick : undefined}
+            >
+              <MessageCardContent
+                dispatch={dispatch}
+                field={component}
+                name={item}
+                classes={classes}
+                emailTemplates={emailTemplates}
+                renderVariables={renderVariables}
+                form={form}
+                disabled={isInternal}
+              />
+            </ScriptCard>
+          </div>
+        );
+      }
+      case "Report": {
+        return (
+          <div ref={provided.innerRef} {...provided.draggableProps}>
+            <ScriptCard
+              heading="Report"
+              className="mb-3"
+              onDelete={!isInternal ? e => onDelete(e, index) : null}
+              dragHandlerProps={provided.dragHandleProps}
+              expanded
+              onDetailsClick={isInternal ? onInternalSaveClick : undefined}
+            >
+              <ReportCardContent
+                dispatch={dispatch}
+                field={component}
+                name={item}
+                form={form}
+                disabled={isInternal}
+                renderVariables={renderVariables}
+              />
+            </ScriptCard>
+          </div>
+        );
+      }
+      default:
+        return null;
+    }
+  };
+
+  const Row = (item, index) => {
+    const component: ScriptComponent = fields.get(index);
+    return (
+      <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
+        {provided => <Item component={component} provided={provided} index={index} item={item} />}
+      </Draggable>
+    );
+  };
+
+  // const RowVirtualised = ({ index, style }) => {
+  //   const component: ScriptComponent = fields.get(index);
+  //   const item = `${fields.name}[${index}]`;
+  //   return (
+  //     <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
+  //       {provided => <Item component={component} provided={provided} index={index} item={item} style={style} />}
+  //     </Draggable>
+  //   );
+  // };
 
   return (
     <DragDropContext onDragEnd={args => onDragEnd({ ...args, fields })}>
-      <Droppable droppableId="droppable">
+      <Droppable
+        droppableId="droppable"
+        // mode="virtual" renderClone={(provided,snapshot,rubric)=>(<Item component={fields.get(rubric.source.index)} provided={provided} index={rubric.source.index} item={`${fields.name}[${rubric.source.index}]`} />)}
+      >
         {provided => (
+          // <List height={700} itemCount={fields.length} itemSize={200} width={300} outerRef={provided.innerRef}>{Row}</List>
           <div ref={provided.innerRef} className="pt-3 pb-3">
-            {fields.map((item, index) => {
-              const component: ScriptComponent = fields.get(index);
-
-              switch (component.type) {
-                case "Script": {
-                  return (
-                    <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
-                      {provided => (
-                        <div ref={provided.innerRef} {...provided.draggableProps}>
-                          <ScriptCard
-                            heading="Script"
-                            className="mb-3"
-                            onDelete={!isInternal && hasUpdateAccess ? e => onDelete(e, index) : null}
-                            dragHandlerProps={provided.dragHandleProps}
-                            expanded
-                            noPadding
-                            onDetailsClick={isInternal ? onInternalSaveClick : undefined}
-                          >
-                            <FormField
-                              type="code"
-                              name={`${item}.content`}
-                              disabled={isInternal || !hasUpdateAccess}
-                              className="mt-3"
-                            />
-                          </ScriptCard>
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                }
-                case "Query": {
-                  return (
-                    <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
-                      {provided => (
-                        <div ref={provided.innerRef} {...provided.draggableProps}>
-                          <ScriptCard
-                            heading="Query"
-                            className="mb-3"
-                            onDelete={!isInternal ? e => onDelete(e, index) : null}
-                            dragHandlerProps={provided.dragHandleProps}
-                            expanded
-                            onDetailsClick={isInternal ? onInternalSaveClick : undefined}
-                          >
-                            <QueryCardContent
-                              dispatch={dispatch}
-                              field={component}
-                              name={item}
-                              classes={classes}
-                              disabled={isInternal}
-                            />
-                          </ScriptCard>
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                }
-                case "Message": {
-                  return (
-                    <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
-                      {provided => (
-                        <div ref={provided.innerRef} {...provided.draggableProps}>
-                          <ScriptCard
-                            heading="Message"
-                            className="mb-3"
-                            onDelete={!isInternal ? e => onDelete(e, index) : null}
-                            dragHandlerProps={provided.dragHandleProps}
-                            expanded
-                            onDetailsClick={isInternal ? onInternalSaveClick : undefined}
-                          >
-                            <MessageCardContent
-                              dispatch={dispatch}
-                              field={component}
-                              name={item}
-                              classes={classes}
-                              emailTemplates={emailTemplates}
-                              renderVariables={renderVariables}
-                              form={form}
-                              disabled={isInternal}
-                            />
-                          </ScriptCard>
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                }
-
-                case "Report": {
-                  return (
-                    <Draggable key={component.id} draggableId={index + component.id} index={index} isDragDisabled={isInternal}>
-                      {provided => (
-                        <div ref={provided.innerRef} {...provided.draggableProps}>
-                          <ScriptCard
-                            heading="Report"
-                            className="mb-3"
-                            onDelete={!isInternal ? e => onDelete(e, index) : null}
-                            dragHandlerProps={provided.dragHandleProps}
-                            expanded
-                            onDetailsClick={isInternal ? onInternalSaveClick : undefined}
-                          >
-                            <ReportCardContent
-                              dispatch={dispatch}
-                              field={component}
-                              name={item}
-                              form={form}
-                              disabled={isInternal}
-                              renderVariables={renderVariables}
-                            />
-                          </ScriptCard>
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                }
-                default:
-                  return null;
-              }
-            })}
+            {fields.map(Row)}
           </div>
         )}
       </Droppable>
